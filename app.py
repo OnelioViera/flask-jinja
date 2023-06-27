e3d41345b8ccf1adf5bc697da817dc38b21db2ac
OnelioViera <ojvwebdesign@gmail.com>
2023-06-27 07:08:20 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        title="Jinja Demo Site",
+        description="Smarter page templates with Flask & Jinja",
+    )
